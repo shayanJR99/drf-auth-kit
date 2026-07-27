5021f2a8c278df18df72ea2c37694c2fd1661424
shayanJR99 <shahrbandishayan@gmail.com>
2026-07-27 20:55:05 +0330

    shayanJR99: connect vite-react to project

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "rest_framework_simplejwt.token_blacklist",
+    "corsheaders",
+
     # Local apps
     "authentication",
     "emails",
@@ -55,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -206,3 +209,13 @@ SPECTACULAR_SETTINGS = {
 # --- Media files (User uploaded files) ---
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
